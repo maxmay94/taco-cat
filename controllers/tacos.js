ac5404b7c8bdbc9da892310850f716203f233241
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-03-03 16:01:23 -0600

    making tacos wooooot

--- controllers/tacos.js
+++ controllers/tacos.js
@@ -1,9 +1,33 @@
 import { Taco } from "../models/taco.js"
 
 function index(req, res) {
-  console.log("TACOS")
+  Taco.find({})
+  .then(tacos => {
+    res.render('tacos/index', {
+      tacos,
+      title: "🌮"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/tacos")
+  })
+}
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+  req.body.tasty = !!req.body.tasty
+  Taco.create(req.body)
+  .then(taco => {
+    res.redirect('/tacos')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/tacos")
+  })
 }
 
 export {
-  index
+  index,
+  create
 }
